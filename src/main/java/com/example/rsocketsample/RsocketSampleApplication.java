2826b9c34343cfdea2f2d1a6a84b417fbdfbd719
glenn <glenn.jo@kakaocorp.com>
2020-10-05 21:39:51 +0900

    first commit

---
+++ src/main/java/com/example/rsocketsample/RsocketSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.rsocketsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsocketSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rsocketsample/RsocketSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rsocketsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
